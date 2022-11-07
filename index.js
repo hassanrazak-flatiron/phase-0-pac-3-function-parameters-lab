d19acc1d61987cbbc8aed98bc315e663e0fe02d6
Hassan Razak <hassanrazak1991@gmail.com>
2022-11-07 09:22:32 -0500

    commit tree 5a4cb13e4bcbb2adb149b342b735bbc66ff1d175

---
+++ index.js
@@ -0,0 +1,13 @@
+function introduction(name){
+    return `Hi, my name is ${name}.`;
+}
+
+//console.log(introduction("hassan"));
+
+function introductionWithLanguage(name, language){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+function introductionWithLanguageOptional(name = "User", language = "JavaScript"){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
